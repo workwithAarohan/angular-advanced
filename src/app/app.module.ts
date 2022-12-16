f57387477cbbf92157029f5df9e3bb1bfac1d45e
Nakarmi <aarohan.nakarmi@cotiviti.com>
2022-12-16 11:05:05 +0545

    Lazy Loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TodoModule } from './forms/todo/todo.module';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { todoReducer } from './forms/todo/store/todos.reducer';
-import { HeaderComponent } from './components/header/header.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { HeaderModule } from './components/header/header.module';
 import { VoidComponent } from './components/void/void.component';
@@ -25,7 +22,6 @@ import { VoidComponent } from './components/void/void.component';
     AppRoutingModule,
     StoreModule.forRoot({}),
     BrowserAnimationsModule,
-    TodoModule,
     MatCardModule,
     MatFormFieldModule,
     HeaderModule,
